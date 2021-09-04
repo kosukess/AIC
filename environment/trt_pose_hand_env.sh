7e3486c2abe11e7fdb65699a47a2720538935699
Tohru Takechi <tohru-takechi@outlook.com>
2021-09-04 18:09:24 +0900

    docker update

--- environment/trt_pose_hand_env.sh
+++ environment/trt_pose_hand_env.sh
@@ -7,10 +7,6 @@ python3 setup.py install --plugins
 
 cd ~
 
-# install dependency libraries
-python3 -m pip install tqdm cython pycocotools
-apt-get install python3-matplotlib
-
 # install trt_pose
 git clone https://github.com/NVIDIA-AI-IOT/trt_pose
 cd trt_pose
