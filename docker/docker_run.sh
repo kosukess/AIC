8c7bf81d37e327f1ff5dbc2bef365d299cff7ca3
Tohru Takechi <tohru-takechi@outlook.com>
2021-09-04 16:51:31 +0900

    docker update

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,2 +1,2 @@
 PATH_DIR_PARENT="$(dirname "$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)")"
-sudo docker run -it --runtime nvidia -p 8000:8000 -v "$PATH_DIR_PARENT":/home --name aic_project_run aic_project:develop
+sudo docker run -it --runtime nvidia -p 8000:8000 -v "$PATH_DIR_PARENT":/root --name aic_project_run aic_project:develop
