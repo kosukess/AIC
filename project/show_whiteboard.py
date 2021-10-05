dffd92065bf90c09dcf50f7bfc15c0ba82897abc
kosuke <k-sakano@keio.jp>
2021-10-05 23:08:12 +0900

    c

--- project/show_whiteboard.py
+++ project/show_whiteboard.py
@@ -2,8 +2,9 @@ import numpy as np
 import cv2
 
 
+size = [hight, width, 3]
 
-img_white = np.ones
+img_white = np.full(size, fill_values = 255, dtype ='unit8')
 
 #for py
 cv2.namedWindow('white board', cv2.WINDOW_NORMAL)
@@ -11,3 +12,7 @@ cv2.imshow('white board', img_white)
 
 
 #for ipynb
+import cv2
+from google.colab.patches import cv2_imshow
+
+cv2_imshow(img_white)
