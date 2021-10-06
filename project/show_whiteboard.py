5470b1d5678b02de81a7e6f3b72372ee38f38f35
kosuke <k-sakano@keio.jp>
2021-10-06 23:48:00 +0900

    made file

--- project/show_whiteboard.py
+++ project/show_whiteboard.py
@@ -4,10 +4,9 @@ import cv2
 
 size = [hight, width, 3]
 
-img_white = np.full(size, fill_values = 255, dtype ='unit8')
+img_white = np.full(size, fill_values = 255, dtype ='uint8')
 
-#for py
 cv2.namedWindow('white board', cv2.WINDOW_NORMAL)
 cv2.imshow('white board', img_white)
-cv2.waitKey(1)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
