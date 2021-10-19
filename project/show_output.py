95267ebd08ce0699d2da1a5f6ca9836b774a6ac3
Tohru Takechi <tohru-takechi@outlook.com>
2021-10-19 22:26:48 +0900

    finish eraser and size

--- project/show_output.py
+++ project/show_output.py
@@ -1,21 +1,19 @@
 from whiteboard import Whiteboard
 import sys
 import signal
+import numpy as np
 
 
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     wb = Whiteboard(h=480, w=640)
-    key = wb.show_whiteboard()
-    if  key == ord('q'):
-        wb.sock.close()
-        sys.exit()
 
     while True:
-        data = wb.receive_data()
-        #wb.switch(data[1])
-        wb.draw(data[0], data[1])
-        key = wb.show_whiteboard()
+        cursor_data, gesture = wb.receive_data()
+        cursor = wb.cursor_process(cursor_data)
+        wb.execute_function(cursor, gesture)
+        key = wb.show_whiteboard(cursor)
+        wb.loop_finish_process(cursor, gesture)
         if  key == ord('q'):
             wb.sock.close()
             break
